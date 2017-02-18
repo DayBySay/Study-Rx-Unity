de0512b6ec0f39fc6a2e17b628133ea0ae782be4
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-02-18 13:45:44 +0900

    タイマーの追加

---
+++ Assets/TimeCounter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeCounter : MonoBehaviour {
+    public delegate void TimerEventHandler(int time);
+    public TimerEventHandler timerEventHandler;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(TimerCoroutine());		
+	}
+
+    IEnumerator TimerCoroutine()
+    {
+        var time = 100;
+        while (time > 0)
+        {
+            time--;
+            timerEventHandler(time);
+
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
