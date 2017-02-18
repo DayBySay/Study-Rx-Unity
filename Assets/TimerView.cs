df020cda074257a426a8ce8520966d1bde6e6d42
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-02-18 13:49:49 +0900

    タイマー表示の実装

---
+++ Assets/TimerView.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerView : MonoBehaviour {
+
+    [SerializeField]
+    private TimeCounter counter;
+    [SerializeField]
+    private Text text;
+	// Use this for initialization
+	void Start () {
+        counter.timerEventHandler = (time) =>
+        {
+            text.text = time.ToString();
+        };
+	}
+}
